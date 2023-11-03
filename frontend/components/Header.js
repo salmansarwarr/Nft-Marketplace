9af0c5559b891e1053342b058c39371679a16428
Muhammad Salman Sarwar <muhammadsalmansarwar32@gmail.com>
2023-11-03 20:13:31 +0500

    improve styling

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -8,13 +8,16 @@ const Header = () => {
         <nav className="p-5 border-b-2 flex flex-row justify-between items-center">
             <h1 className="p-4 font-bold text-3xl">NFT Marketplace</h1>
             <div className="flex flex-row items-center">
-                <Link className="mr-4 p-6 hover:underline" href="/">Home</Link>
-                <Link className="mr-4 p-6 hover:underline" href="/sell-nft">Sell NFT</Link>
-                <ConnectButton moralisAuth={false}/>
+                <div className="mr-4 p-6 hover:underline">
+                    <Link href="/">Home</Link>
+                </div>
+                <div className="mr-4 p-6 hover:underline">
+                    <Link href="/sell-nft">Sell NFT</Link>
+                </div>
+                <ConnectButton moralisAuth={false} />
             </div>
         </nav>
     );
 };
 
 export default Header;
-
